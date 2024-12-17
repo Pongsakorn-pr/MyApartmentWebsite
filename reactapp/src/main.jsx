e7e2ea848f008b2cbc28baedc28abbf806081870
Pongsakorn-pr <pongsakorn_pr@kkumail.com>
2024-12-17 09:02:50 +0700

    Add Navbar/ DashBoard Page

--- reactapp/src/main.jsx
+++ reactapp/src/main.jsx
@@ -1,12 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App'
-import Mynavbar from './Navbar'
+import App from './App'10
+import Navbar from './Navbar'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <App /> 
     </StrictMode>
 );
-
