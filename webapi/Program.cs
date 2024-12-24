c7daae6ef695034644d73081e1cd2291a4f72c20
Pongsakorn-pr <pongsakorn_pr@kkumail.com>
2024-12-24 16:00:53 +0700

    commit new Change

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -1,23 +1,35 @@
 using webapi.Model;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<GoogleSheetsHelper>();  // Example of a singleton service
+builder.Services.AddSingleton<GoogleSheetsHelper>();
 
 var app = builder.Build();
 
-// Apply CORS and other middleware
-app.UseCors("AllowReactApp");
-
-app.Use(async (context, next) =>
+app.UseCors(policy =>
 {
-    context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
-    context.Response.Headers.Add("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
-    context.Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
-    await next.Invoke();
+    policy.WithOrigins("http://localhost:3000") // Replace with your React app's URL during development
+          .AllowAnyHeader()
+          .AllowAnyMethod();
 });
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath, @"E:\Web\Solution1\reactapp\dist")),
+    RequestPath = ""
+});
+
+app.MapFallbackToFile("index.html", new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath, @"E:\Web\Solution1\reactapp\dist"))
+});
+
 
 if (app.Environment.IsDevelopment())
 {
